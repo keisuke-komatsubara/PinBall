053d3765b70b231caf5c4b3d3b8324b95ca3a98b
小松原桂介 <kk.kabocha@outlook.com>
2020-01-27 23:38:24 +0900

    [remove]スクリプトの整理
    score表示のコードをballcontrollerに移してscoreのスクリプトを削除しました
    タッチ操作のコードをFripperContorollerに移してタッチ操作用のスクリプトを削除しました

--- Assets/Ballcontroller.cs
+++ Assets/Ballcontroller.cs
@@ -5,25 +5,68 @@ using UnityEngine.UI;
 
 public class Ballcontroller : MonoBehaviour {
 
+    //SmallStarの得点
+    public int smallstarpoint = 10;
+    //LargeStarの得点
+    public int largestarpoint = 20;
+    //SmallCloudの得点
+    public int smallcloudpoint = 30;
+    //LargeCloudの得点
+    public int largecloudpoint = 50;
+
+    private GameObject ScoreText;
+    private int scorenum = 0;
+
+    //衝突時に呼び出される関数
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            scorenum += this.smallstarpoint;
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            scorenum += this.largestarpoint;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            scorenum += this.smallcloudpoint;
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            scorenum += this.largecloudpoint;
+        }
+    }
+    
     //ボールが見える可能性のあるz軸の最大値
     private float visiblePosZ = -6.5f;
 
     //ゲームオーバを表示するテキスト
     private GameObject gameoverText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         //シーン中のGameOvertextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//ボールが画面外に出た場合
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Scoreを表示する
+        this.ScoreText.GetComponent<Text>().text = "SCOORE:" + scorenum;
+
+        //ボールが画面外に出た場合
         if (this.transform.position.z < this.visiblePosZ)
         {
             //GameoverTextにゲームオーバーを表示
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
-	}
+    }
 }
+
+
+
--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -44,7 +44,48 @@ public class FripperController : MonoBehaviour {
         {
             SetAngle(this.defaultAngle);
         }
-	}
+
+        //タッチ操作
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                //タッチ情報を取得
+                Touch touch = Input.GetTouch(i);
+                //タッチ座標を取得
+                float x = touch.position.x;
+                float y = touch.position.y;
+
+                if (x <= Screen.width / 2)
+                {
+                    //左半分をタッチしたとき左フリッパーを動かす
+                    if (touch.phase == TouchPhase.Began && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+                    //左半分を離したとき左フリッパーを元に戻す
+                    if (touch.phase == TouchPhase.Ended && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                }
+                else if (x >= Screen.width / 2)
+                {
+                    //右半分をタッチしたとき右フリッパーを動かす
+                    if (touch.phase == TouchPhase.Began && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+                    //右半分を離したとき右フリッパーを元に戻す
+                    if (touch.phase == TouchPhase.Ended && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                }
+            }
+        }
+
+    }
 
     //フリッパーの傾きを設定
     public void SetAngle (float angle)
